228dff3cf41a45f6f04ce3c4a46cba46afb36d87
jordan-gerard <jordan.gerard@shopify.com>
2020-03-20 09:04:46 -0400

    improved logging, added siblings, improved format

--- test.js
+++ test.js
@@ -2,64 +2,122 @@
 let log = false;
 // Target
 let sTarget = "#test";
-let eTarget = document.querySelector(sTarget);
-let rTarget = {
-    x: eTarget.getBoundingClientRect().x,
-    y: eTarget.getBoundingClientRect().y
+// Prevent further execution if target is not found
+if (!document.querySelector(sTarget)) {
+  // creates a new exception type:
+  function FatalError() {
+    Error.apply(this, arguments);
+    this.name = "FatalError";
+  }
+  FatalError.prototype = Object.create(Error.prototype);
+
+  // and then, use this to trigger the error:
+  throw new FatalError("Element not found.");
+}
+
+// Main object with target, sibling, and parent data
+let oTarget = {
+  element : document.querySelector(sTarget),
+  rect    : {
+    x: document.querySelector(sTarget).getBoundingClientRect().x,
+    y: document.querySelector(sTarget).getBoundingClientRect().y
+  },
+  parents : [],
+  siblings: []
 };
 // Get all parents of target
-let eTargetParents = [];
-if (!eTarget.parentElement) {
-    if (log) console.log("target has no parent elements...");
-    eTargetParents = null;
-} else {
-    let t = eTarget;
-    while (t.parentElement) {
-        if (log) console.log(`target has a parent element...`);
-        if (log) console.info(t.parentElement);
-        eTargetParents.push(t.parentElement);
-        t = t.parentElement;
-    }
-}
-let rElements = [];
-for (let i = 0; i < eTargetParents.length; i++) {
-    let rect = {
-        x: eTargetParents[i].getBoundingClientRect().x,
-        y: eTargetParents[i].getBoundingClientRect().y
-    };
-    rElements.push({
-        element: eTargetParents[i],
-        rect: rect
+if (!oTarget.element.parentElement) {
+  if (log) 
+    console.log(`${oTarget.element} has no parent elements...`);
+  }
+else {
+  let {element} = oTarget;
+  while (element.parentElement) {
+    if (log) 
+      console.log(`${element} has a parent element...`);
+    if (log) 
+      console.info(element.parentElement);
+    oTarget.parents.push({
+      element: element.parentElement,
+      rect   : {
+        x: element.parentElement.getBoundingClientRect().x,
+        y: element.parentElement.getBoundingClientRect().y
+      }
     });
+    element = element.parentElement;
+  }
 }
-if (rElements.length > 0) {
-    if (log) console.info(rElements);
+// Get all siblings of target
+if (!oTarget.element.parentElement) {
+  if (log) 
+    console.log(`${oTarget.element} has no parent element, cannot determine siblings (likely has none as it is the HTML root element)...`)
+} else {
+  let element = oTarget.element.parentElement.firstElementChild;
+  while (element) {
+    if (element !== oTarget.element) {
+      if (log) 
+        console.log(`${element} is a sibling element...`);
+      if (log) 
+        console.info(element);
+      oTarget.siblings.push({
+        element,
+        rect: {
+          x: element.getBoundingClientRect().x,
+          y: element.getBoundingClientRect().y
+        }
+      });
+      element = element.nextElementSibling;
+    } else {
+      break;
+    }
+  }
 }
+
+// Run timers after DOM has loaded
 document.addEventListener("DOMContentLoaded", function() {
-    startTimers();
+  startTimers();
 });
 
-function getRect(el) {
-    return el.getBoundingClientRect();
-}
+// Timer functions
+//
 
+// Begins all timers (after DOM loads)
 function startTimers() {
-    startTimer(rTarget, eTarget);
-    for (let i = 0; i < rElements.length; i++) {
-        startTimer(rElements[i].rect, rElements[i].element);
-    }
+  // Self first
+  startTimer(oTarget.rect, oTarget.element);
+  // Siblings
+  for (let i = 0; i < oTarget.siblings.length; i++) {
+    startTimer(oTarget.siblings[i].rect, oTarget.siblings[i].element);
+  }
+  // Parents
+  for (let i = 0; i < oTarget.parents.length; i++) {
+    startTimer(oTarget.parents[i].rect, oTarget.parents[i].element);
+  }
 }
 
+// (re)Starts a specific timer
 function startTimer(reference, element) {
-    setTimeout(checkRect.bind(null, reference, element), 1000);
+  setTimeout(checkRect.bind(null, reference, element), 1000);
 }
 
+// Helper functions
+//
+
+// Returns current rectangle
+function getRect(el) {
+  return el.getBoundingClientRect();
+}
+
+// Checks if reference rectangle is the same as target rectangle
 function checkRect(reference, element) {
-    if (reference.x != getRect(element).x || reference.y != getRect(element).y) {
-        console.log(`Element: ${element}  -  Rect: (x: ${reference.x}, y: ${reference.y})  -  HAS MOVED!`);
-        console.log(`New rect: (x: ${getRect(element).x}, y: ${getRect(element).y})`);
-    } else {
-        if (log) console.log(`Element: ${element}  -  has NOT moved...`);
-        startTimer(reference, element);
-    }
+  if (reference.x != getRect(element).x || reference.y != getRect(element).y) {
+    console.groupCollapsed(`${element}`);
+    console.log(element);
+    console.log(`Old rect: (x: ${reference.x}, y: ${reference.y})  |  New rect: (x: ${getRect(element).x}, y: ${getRect(element).y})`);
+    console.groupEnd();
+  } else {
+    if (log) 
+      console.log(`Element: ${element}  -  has NOT moved...`);
+    startTimer(reference, element);
+  }
 }
